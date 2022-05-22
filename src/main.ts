355395db07043e1772a9b589c31baa47c09fac4b
Librariann <okpc0306@naver.com>
2022-05-23 00:26:17 +0900

    add comment

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-      transform: true,
+      transform: true, //데이터타입 자동변환.. ex) get :id로 받는 id값은 무조껀 string인데 이때 number로 변환해서 받을수있다 코드절약됨
     }),
   );
   await app.listen(3000);
